a654db75b1228b15442a4c61e8f768dc99cedcdc
nomnomnonono <murumuru1204@gmail.com>
2023-09-12 11:04:00 +0900

    add blog resouce

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :blogs
   devise_for :users
 
   devise_scope :users do
     get '/users', to: redirect("/users/sign_up")
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  root 'blogs#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_123150) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_015247) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
